281487b89279ed6043932324ca728566e244b310
git config --global AlessandroADP <alessandro.davila@tecsup.edu.pe>
2025-05-29 14:16:17 -0500

    vet service

--- src/main/java/com/tecsup/petclinic/services/VetService.java
+++ src/main/java/com/tecsup/petclinic/services/VetService.java
@@ -1,4 +1,23 @@
 package com.tecsup.petclinic.services;
 
-public class VetService {
+import com.tecsup.petclinic.entities.Vet;
+import com.tecsup.petclinic.exception.VetNotFoundException;
+
+import java.util.List;
+
+public interface VetService {
+
+    Vet create (Vet vet);
+
+    void delete (Integer id) throws VetNotFoundException;
+
+    List<Vet> findAll();
+
+    List<Vet> findByLastName(String lastName);
+
+    List<Vet> findByFirstName(String firstname);
+
+    Vet update (Vet vet);
+
+
 }
