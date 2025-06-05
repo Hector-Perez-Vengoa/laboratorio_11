70f1445b6a30e5b4337b06b3144dc640189cac3c
Hector-Perez-Vengoa <hector.perez@tecsup.edu.pe>
2025-06-05 15:04:43 -0500

    buscar

--- src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -1,4 +1,48 @@
 package com.tecsup.petclinic.webs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.tecsup.petclinic.dtos.VetDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Slf4j
 public class VetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testFindVetOK() throws Exception {
+        int VET_ID = 3;
+        String VET_FIRST_NAME = "Linda"; // Ajusta según tu base de datos
+        String VET_LAST_NAME = "Douglas"; // Ajusta según tu base de datos
+
+        this.mockMvc.perform(get("/vets/1"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(VET_ID))
+                .andExpect(jsonPath("$.firstName").value(VET_FIRST_NAME))
+                .andExpect(jsonPath("$.lastName").value(VET_LAST_NAME));
+    }
+
+    @Test
+    public void testFindVetKO() throws Exception {
+        mockMvc.perform(get("/vets/666"))
+                .andExpect(status().isNotFound());
+    }
+
 }
