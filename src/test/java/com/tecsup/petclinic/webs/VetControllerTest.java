f47f8966a58b4bb9c6218829e91f9a284814f12d
Diego <luis.quintana.g@tecsup.edu.pe>
2025-06-05 15:21:39 -0500

    create test

--- src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VetControllerTest.java
@@ -1,7 +1,5 @@
 package com.tecsup.petclinic.webs;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
 import com.tecsup.petclinic.dtos.VetDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -11,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -22,27 +19,52 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Slf4j
 public class VetControllerTest {
 
+    private static final ObjectMapper om = new ObjectMapper();
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void testFindVetOK() throws Exception {
-        int VET_ID = 3;
-        String VET_FIRST_NAME = "Linda"; // Ajusta según tu base de datos
-        String VET_LAST_NAME = "Douglas"; // Ajusta según tu base de datos
+
+        int VET_ID = 1;
+        String VET_FIRST_NAME = "James";
+        String VET_LAST_NAME = "Carter";
 
         this.mockMvc.perform(get("/vets/1"))
+                .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(VET_ID))
                 .andExpect(jsonPath("$.firstName").value(VET_FIRST_NAME))
                 .andExpect(jsonPath("$.lastName").value(VET_LAST_NAME));
-    }
-
-    @Test
-    public void testFindVetKO() throws Exception {
-        mockMvc.perform(get("/vets/666"))
+    }    @Test
+    public void testFindVetNotFound() throws Exception {
+        mockMvc.perform(get("/vets/999"))
+                .andDo(print())
                 .andExpect(status().isNotFound());
     }
 
+    /**
+     * Prueba crear veterinario
+     * @throws Exception
+     */
+    @Test
+    public void testCreateVet() throws Exception {
+
+        String VET_FIRST_NAME = "Dr. House";
+        String VET_LAST_NAME = "Wilson";
+
+        VetDTO newVetTO = new VetDTO();
+        newVetTO.setFirstName(VET_FIRST_NAME);
+        newVetTO.setLastName(VET_LAST_NAME);
+
+        this.mockMvc.perform(post("/vets")
+                        .content(om.writeValueAsString(newVetTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(VET_FIRST_NAME)))
+                .andExpect(jsonPath("$.lastName", is(VET_LAST_NAME)));
+    }
 }
